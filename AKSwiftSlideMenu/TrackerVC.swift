9c1e0003d16a0e1c1518bf8e5af8db410e3abceb
Chris Bodger <ChrisBodger@Chriss-MacBook-Pro-2.local>
2017-01-24 16:55:12 +0000

    Initial Commit

---
+++ AKSwiftSlideMenu/TrackerVC.swift
@@ -0,0 +1,34 @@
+//
+//  TrackerVC.swift
+//  AKSwiftSlideMenu
+//
+//  Created by Chris Bodger on 10/11/2016.
+//
+
+import UIKit
+
+class TrackerVC: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSlideMenuButton()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
